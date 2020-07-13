f2ede9c755800a9df0152c15ea1304ba2af70306
Walid Amriou <walidamriou@gmail.com>
2020-07-13 17:35:07 +0200

    Update

--- 001_Getting_Started/001_Hello_World/main.go
+++ 001_Getting_Started/001_Hello_World/main.go
@@ -1,10 +1,20 @@
 /*
 Lokum GoLang
 Learn Go Programming Language with small programs examples
-----------
-by Walid Amriou
-https://lokumgolang.walidamriou.com
----------
+/*
+ ********************************************************************
+
+  LokumGoLang
+  To Learn Go Programming Language with small programs examples
+  Project Website: LokumGoLang.walidamriou.com
+  Github: https://github.com/walidamriou/LokumGoLang
+
+  Copyright CC 2020 Walid Amriou
+  This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+
+  Last update: July 2020
+
+ ********************************************************************
 */
 
 /*
