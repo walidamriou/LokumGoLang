cfc4c0c4d0a65787f446546a24b1ca5763273e0c
Walid Amriou <walidamriou@gmail.com>
2020-08-02 22:50:33 +0200

    Upate 001_Getting_Started/001_Hello_World/main.go

--- 001_Getting_Started/002_Variables/main.go
+++ 001_Getting_Started/002_Variables/main.go
@@ -1,10 +1,15 @@
 /*
-Lokum GoLang
-Learn Go Programming Language with small programs examples
-----------
-by Walid Amriou
-https://lokumgolang.walidamriou.com
-----------
+ ******************************************************************** 
+  Lokum GoLang
+  To Learn Go Programming Language with small programs examples
+
+  Project Website: lokumgolang.walidamriou.com
+  Github: https://github.com/walidamriou/LokumGoLang
+  Copyright CC 2020 Walid Amriou
+  This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+   
+  Last update: August 2020
+ ******************************************************************** 
 */
 
 package main
--- 001_Getting_Started/004_Data_types/main.go
+++ 001_Getting_Started/004_Data_types/main.go
@@ -11,22 +11,3 @@
   Last update: August 2020
  ********************************************************************
 */
-
-package main
-
-import "fmt"
-
-// define pi with float32 type
-const pi float32 = 3.1415926
-
-// define 2020 without type
-const year = 2020
-
-func main() {
-	// we use %f to print float value
-	fmt.Printf("The pi is =%f \n", pi)
-
-	//we know that 2020 is int type
-	fmt.Printf("The year is =%d \n", year)
-
-}
